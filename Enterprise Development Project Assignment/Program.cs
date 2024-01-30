7b123bdca386da341d0c80ba5d8f4aa053fadfc8
yuanx1n <liyuanxin682@gmail.com>
2024-01-31 02:55:42 +0800

    Revert "tried to integrate by copy pasting"
    This reverts commit bf8406f5743779452c2a6884e47a6b83655a2f3f.

--- Enterprise Development Project Assignment/Program.cs
+++ Enterprise Development Project Assignment/Program.cs
@@ -1,8 +1,6 @@
 using Enterprise_Development_Project_Assignment;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-
 using Microsoft.OpenApi.Models;
 using AutoMapper;
 var builder = WebApplication.CreateBuilder(args);
@@ -10,11 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<MyDbContext>();
-// Auto Mapper
 
 var mappingConfig = new MapperConfiguration(mc =>
 {
-    mc.AddProfile(new MappingProfile());
+   
 });
 IMapper mapper = mappingConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
@@ -25,57 +22,17 @@ var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<stri
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.WithOrigins(allowedOrigins)
-         .AllowAnyMethod()
-         .AllowAnyHeader();
-        });
-});
-
-// Authentication
-var secret = builder.Configuration.GetValue<string>("Authentication:Secret");
-builder.Services
-.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters()
+    policy =>
     {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(
-    Encoding.UTF8.GetBytes(secret)
-    ),
-    };
+        policy.WithOrigins(allowedOrigins)
+     .AllowAnyMethod()
+     .AllowAnyHeader();
+    });
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
-    options.AddSecurityDefinition("Bearer", securityScheme);
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        { securityScheme, new List<string>() }
-    });
-});
-
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -88,10 +45,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors();
-app.UseStaticFiles();
 
 app.UseAuthorization();
-app.UseAuthentication();
 
 app.MapControllers();
 
