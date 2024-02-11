bc77e95b6917edd9e2d9d5bceb0ff0091592ab1e
Vincent Yen <dogeandshibe@gmail.com>
2024-02-11 16:11:54 +0800

    syncing latest updates

--- Enterprise Development Project Assignment/Migrations/20240211080048_Init.cs
+++ Enterprise Development Project Assignment/Migrations/20240211080048_Init.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Enterprise_Development_Project_Assignment.Migrations
 {
     /// <inheritdoc />
-    public partial class date : Migration
+    public partial class Init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
